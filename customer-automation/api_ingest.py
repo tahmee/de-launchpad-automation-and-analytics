b641f63522495bc5f37f6f8b16a6d2299eb75d7e
tahmee <susantamunokubie@gmail.com>
2025-11-01 09:05:43 +0100

    test retrieval

--- customer-automation/api_ingest.py
+++ customer-automation/api_ingest.py
@@ -14,13 +14,14 @@ logger = logging.getLogger(__name__)
 # Assign Zenquotes (quote only) api url to a variable zp_api
 zq_api = "https://zenquotes.io/api/quotes/"
 
-# Create function to connect to api and fetch data
+# Define function to connect to api and fetch data
 def fetch_api_data(url):
     """ This function takes the api url as argument.
     Connects to the api, fetches the data and parses it to JSON format.
     Returns a json format data.
     """
     try:
+        logger.info("BEGIN------------------------------")
         logger.info(f"Attempting to fetch data from {url}")
         response = requests.get(url, timeout=10)
         logger.info(f"Response status code: {response.status_code}")
@@ -54,3 +55,9 @@ def fetch_api_data(url):
         logger.error(f"An unexpected error occurred: {str(e)}", exc_info =True)
         return None
 
+# Call fetch_api_data function and store value in variable
+quote_data = fetch_api_data(zq_api)
+
+# Define function to save data
+print(type(quote_data))
+print(quote_data[0])
